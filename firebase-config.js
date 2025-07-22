9e5afca41d1ca0947e0ffd099d93f01b4ee36f38
Codo <71037116+codo-coderson@users.noreply.github.com>
2025-07-22 17:25:44 +0200

    Rename app to MonitorAula

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // firebase-config.js
 export const firebaseConfig = { 
   apiKey: "AIzaSyCsWVffr6yvIZel2Wzhy1v9ZtvKPiMqiFQ", 
-  authDomain: "controlpipiapp.firebaseapp.com", 
-  projectId: "controlpipiapp", 
-  storageBucket: "controlpipiapp.firebasestorage.app", 
+  authDomain: "monitoraula.firebaseapp.com",
+  projectId: "monitoraula",
+  storageBucket: "monitoraula.firebasestorage.app",
   messagingSenderId: "1059568174856", 
   appId: "1:1059568174856:web:cf9d54881bb07961d60ebd" 
 };
